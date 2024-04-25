93a2f1d84c8a4ae9dd6fa991d93f1e7ab93e7bc6
Gray <96966800+communicationDL1@users.noreply.github.com>
2024-04-25 12:22:08 +0800

    Update V2V_Model.py

--- V2V_Model.py
+++ V2V_Model.py
@@ -27,6 +27,50 @@ def get_flops_params():
     params = tf.compat.v1.profiler.profile(graph, options=tf.compat.v1.profiler.ProfileOptionBuilder.trainable_variables_parameter())
     print('FLOPs: {};    Trainable params: {}'.format(flops.total_float_ops, params.total_parameters))
 KS=9
+def CNN_paper_bndropout():
+    cnn_input = Input(shape=[52, 2])
+    x = Conv1D(32, KS, padding='same')(cnn_input)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Conv1D(32, KS, padding='same')(x)
+    # x = SeparableConv1D(32, KS, padding='same')(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = AveragePooling1D(4)(x)
+    # x = SeparableConv1D(32, KS, padding='same')(x
+    x = Conv1D(32, KS, padding='same')(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    # x = SeparableConv1D(32, KS, padding='same')(x)
+    x = Conv1D(32, KS, padding='same')(x)
+    x = Activation('relu')(x)
+    x = BatchNormalization()(x)
+    x = Flatten()(x)
+    x = Dense(32,activation="gelu", kernel_regularizer=regularizers.l2(0.01))(x)
+    x = BatchNormalization()(x)
+    x = GaussianDropout(0.5)(x)
+    x = Dense(5, activation="softmax", kernel_regularizer=regularizers.l2(0.01))(x)
+    adam=optimizers.Adam(lr=0.001)
+    model = Model(inputs=cnn_input, outputs=x)
+    model.compile(loss='categorical_crossentropy',optimizer=adam, metrics=['accuracy'])
+    return model
+def CNN_paper():
+        cnn_input = Input(shape=[52, 2])
+        x = Conv1D(32, KS, padding='same')(cnn_input)
+        x = Activation('relu')(x)
+        x = SeparableConv1D(32, KS, padding='same')(x)
+        x = Activation('relu')(x)
+        x = AveragePooling1D(4)(x)
+        x = SeparableConv1D(32, KS, padding='same')(x)
+        x = Activation('relu')(x)
+        x = SeparableConv1D(32, KS, padding='same')(x)
+        x = Activation('relu')(x)
+        x = GlobalAveragePooling1D()(x)
+        x = Dense(5, activation="softmax", kernel_regularizer=regularizers.l2(0.01))(x)
+        # SGD = optimizers.SGD()
+        model = Model(inputs=cnn_input, outputs=x)
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
 def DCNN():
     cnn_input = Input(shape=[52, 2])
     x = Conv1D(30, 15, padding='same')(cnn_input)
@@ -50,7 +94,7 @@ def DCNN():
     model = Model(inputs=cnn_input, outputs=x)
     model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
     return model
-data_path="V2V_H_SI.mat"
+data_path="V2V_H.mat"
 data = scio.loadmat(data_path)
 x=data.get('H')
 Sample_num = 10000
@@ -63,9 +107,9 @@ y=np.vstack((y1,y2,y3,y4,y5))
 y = array(y)
 y = to_categorical(y)
 X_train, X_val, Y_train, Y_val = train_test_split(x, y, test_size = 0.3, random_state= 1)
-model = DCNN()
+model = CNN_paper_bndropout()
 model.summary()
-checkpoint = ModelCheckpoint("DCNN.hdf5", verbose=1, save_best_only=False)
+checkpoint = ModelCheckpoint("CNN_GAP_gelu.hdf5", verbose=1, save_best_only=False)
 hist=model.fit(
     X_train,
     Y_train,
@@ -78,5 +122,5 @@ hist=model.fit(
 train_test_list = [hist.history['accuracy'],hist.history['val_accuracy'],hist.history['loss'],hist.history['val_loss']]
 train_test_array=np.array(train_test_list).T
 df = pd.DataFrame(train_test_array, columns=['Training Acc', 'Test Acc','Training Loss','Test Loss'])
-df.to_excel("DCNN.xlsx", index=False)
+df.to_excel("CNN_GAP_gelu.xlsx", index=False)
 
